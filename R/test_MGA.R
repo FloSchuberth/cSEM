652f45a58132a3066bcb52caf196d363ee963b8a
Manuel Steiner <manuel.steiner@uni-wuerzburg.de>
2018-08-02 17:50:12 +0200

    Several changes

--- R/test_MGA.R
+++ R/test_MGA.R
@@ -1,61 +1,26 @@
-#' Test Multigroup
-#' @export
+#' Test for group differences 
 #'
-
-
-# @Distance functions
-calculateArithmDistance <- function(.corMatrices, .distance="geodesic"){
-  # number of groups to compare
-  groups <- length(.corMatrices)
-  # result vector
-  results <- c()
-  # index
-  index <- 1
-  for (g in 2:(groups)) {
-    for(h in 1:(g - 1)){
-      
-      switch(.distance,
-             # Geodesic distance
-             geodesic = {
-               # Eigen values
-               Eigen <- eigen(solve(.corMatrices[[h]]) %*% 
-                                       .corMatrices[[g]])
-               # calc distance
-               results[index]  <-  sum((log(Eigen$values))^2)
-               
-               # results[index] <- 2 * dG(.corMatrices[[h]], .corMatrices[[g]])
-               # update index
-               index <- index + 1 
-             },
-             # Euclidean Distance
-             euclidean = {
-               # Estimate euclidean Distance
-               results[index]  <- sqrt((sum((.corMatrices[[h]] -
-                                               .corMatrices[[g]])^2))/
-                                         (ncol(.corMatrices[[h]])*
-                                            (ncol(.corMatrices[[h]]) + 1))) 
-               index <- index + 1 
-             },
-             # Add other distance measures here
-             
-             # Distance does not exist
-             {
-               stop("This distance measure is not defined.")
-             }
-      )
-    }
-  }
-  res <- (1 / (groups*(groups-1))) * sum(results)
-  return(res)
-}
-
-#' Test statistic to compare group differences. 
+#' Test for group differences.
+#' 
+#' Some more details here including references.
+#' 
+#' @usage testOverllMGA()
+#' 
+#' @param csem_arguments
+#' 
+#' @return csem_testresults
+#' 
+#' @references
+#'   \insertAllCited{}
 #'
-#' @param .objc a cSEM object
-#' @param .runs the number 
-#' @return The test statistic, the critical value, a logical value with 
-#' the decision, and the number of admissible results.
+#' @seealso [csem], [foreman]
+#'
 #' @examples
+#' \dontrun{
+#' # still to implement
+#' }
+#'
+#' @export
 testOverallMGA <- function(.object=args_default()$.model,
                            .dropInadmissibles=args_default()$.dropInadmissibles,
                            .alpha=args_default()$.alpha,
@@ -160,56 +125,3 @@ testOverallMGA <- function(.object=args_default()$.model,
               Decision = decision, 
               Number_admissibles = ncol(ref_dist)))
 }
-
-
-# Test ---------------------------
-
-model <- "
-# Structural model
-EXPE ~ IMAG
-QUAL ~ EXPE
-VAL  ~ EXPE + QUAL
-SAT  ~ IMAG + EXPE + QUAL + VAL
-
-LOY  ~ IMAG + SAT
-
-# Measurement model
-
-IMAG <~ imag1 + imag2 + imag3
-EXPE <~ expe1 + expe2 + expe3
-QUAL <~ qual1 + qual2 + qual3 + qual4 + qual5
-VAL  <~ val1  + val2  + val3
-SAT  <~ sat1  + sat2  + sat3  + sat4
-LOY  <~ loy1  + loy2  + loy3  + loy4
-"
-
-sat.plspm <- read.csv2("C:/Users/Michael Klesel/Dropbox/R Projekte/R cSEM/satisfaction.csv")
-
-require(cSEM)
-data(satisfaction)
-a <- csem(.data = sat.plspm, .model = model, .id = "gender")
-lapply(a, status)
-
-
-testOverallMGA(.object = a, .runs = 20, .alpha = c(0.05, 0.01))
-
-
-
-b <- csem(.data = satisfaction, .model = model, .PLS_weight_scheme_inner = "path")
-
-Test_for_overall_model_fit(b)
-
-
-require(matrixpls)
-c <- matrixpls(S=cor(satisfaction), model = model)
-sum(fitted.matpls(c)[rownames(fitted(b)),colnames(fitted(b))]-fitted(b))
-
-
-
-# MGA
-a_test <- testOverallMGA(a, .10)
-a_test
-
-
-testOverallMGA(a, 5)
-
