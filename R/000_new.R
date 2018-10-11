039b2c0dda9f3438a51db2434a91c0e1ac6c1eb7
FloSchuberth <f.schuberth@utwente.nl>
2018-10-11 14:55:03 +0200

    add references

--- R/000_new.R
+++ R/000_new.R
@@ -20,6 +20,11 @@
 #' # still to implement
 #' }
 #'
+#' @references 
+#' 
+#' Fornell, C. & Larcker, D. F. (1981). Evaluating structural equation models with unobservable variables and measurement error. \emph{Journal of Marketing Research}, 18, 39–50.
+#'
+#'
 #' @export
 #'
 AVE=function(.object=args_default()$.object,
@@ -67,6 +72,10 @@ AVE=function(.object=args_default()$.object,
 #' # still to implement
 #' }
 #'
+#' @references 
+#' 
+#' Raykov, T. (1997). Estimation of composite reliability for congeneric measures. \emph{Applied Psychological Measurement}, 21(2), 173–184.
+#'
 #' @export
 #'
 CR=function(.object=args_default()$.object,
@@ -96,11 +105,10 @@ CR=function(.object=args_default()$.object,
   return(CRs)
 }
 
-#' alpha
+#' Cronbach_alpha
 #'
-#' Compute the composite reliability (CR) based on standardized loading, see Raykov (1997)
 #'
-#' @usage alpha(
+#' @usage Cronbach_alpha(
 #'  .object              = args_default()$.object,
 #'  .only_common_factors = args_default()$.only_common_factors
 #' )
@@ -113,6 +121,10 @@ CR=function(.object=args_default()$.object,
 #' \dontrun{
 #' # still to implement
 #' }
+#'
+#' @references 
+#' 
+#' Cronbach, L. J.(1951). Coefficinet alpha and the internal structure of tests. \emph{Psychometrika}, 16(3), 297–334.
 #'
 #' @export
 #'
@@ -139,5 +151,49 @@ Cronbach_alpha=function(.object=args_default()$.object,
   return(alphas)
 }
 
-
+#' Fornell_Larcker
+#'
+#' Computes the Fornell-Larcker criterion
+#'
+#' @usage Fornell_Larcker(
+#'  .object              = args_default()$.object,
+#'  .only_common_factors = args_default()$.only_common_factors
+#' )
+#'
+#' @inheritParams csem_arguments
+#'
+#' @seealso [csem], [cSEMResults]
+#'
+#' @examples
+#' \dontrun{
+#' # still to implement
+#' }
+#'@references 
+#' 
+#' Fornell, C. & Larcker, D. F. (1981). Evaluating structural equation models with unobservable variables and measurement error. \emph{Journal of Marketing Research}, 18, 39–50.
+#'
+#' @export
+#'
+Fornell_Larcker=function(.object=args_default()$.object,
+                                 .only_common_factors=args_default()$.only_common_factors){
+  construct_names=names(.object$Information$Model$construct_type)
+  
+  # calculate Cronbach's alpah for all constructs
+  alphas=sapply(construct_names,function(x){
+    relevant_indicators=colnames(.object$Information$Model$measurement[x,.object$Information$Model$measurement[x,]!=0,drop=FALSE])
+    S_relevant=.object$Estimates$Indicator_VCV[relevant_indicators,relevant_indicators]
+    alpha_temp=psych::alpha(S_relevant,delete=FALSE,na.rm=FALSE)
+    alpha_temp$total$raw_alpha
+  })
+  
+  names(alphas)=construct_names
+  
+  # By default, for composites the CR is set to 1
+  if(.only_common_factors){
+    co_names=names(.object$Information$Model$construct_type[.object$Information$Model$construct_type=="Composite"])
+    alphas[co_names]=NULL
+  }
+  
+  return(alphas)
+}
 
