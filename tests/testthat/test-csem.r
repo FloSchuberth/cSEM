995af45c9f2526abd3672eca1660da967f575019
Manuel Rademaker <manuel.rademaker@uni-wuerzburg.de>
2019-04-25 13:49:14 +0200

    add test to testthat

---
+++ tests/testthat/test-csem.r
@@ -0,0 +1,17 @@
+context("csem")
+
+model <- "
+# Structural model
+EXPE ~ IMAG
+
+# Measurement model
+
+IMAG <~ imag1 + imag2
+EXPE <~ expe1 + expe2 + expe3
+"
+
+test_that("no data provided should give an error", {
+  expect_error(
+    csem(.model = model)
+  )
+})
