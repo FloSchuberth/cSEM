7a7f21893b0fdd0ab1b56cc8158f599f74a234be
Manuel Rademaker <manuel.rademaker@uni-wuerburg.de>
2019-06-21 12:43:31 +0200

    fix testthat

--- tests/testthat/test-csem.r
+++ tests/testthat/test-csem.r
@@ -1,43 +1,31 @@
 context("csem")
 
-#
-model <- "
-# Structural model
-EXPE ~ IMAG
-
-# Measurement model
-
-IMAG <~ imag1 + imag2
-EXPE <~ expe1 + expe2 + expe3
-"
-
-test_that("No data provided should give an error", {
-  expect_error(
-    csem(.model = model)
-  )
-})
-
-### 2 Common factors ===========================================================
-
-# load(file = "DGP_linear_2commonfactors/1_DGPs.RData") # Loads only Sigma
-# load(file = "DGP_linear_2commonfactors/1_DGPs_models_pop_params.RData") # Loads Sigma and models
-load(file = "Temp/DGPs/DGP_linear_2commonfactors/1_DGPs_models_pop_params.RData") # Loads Sigma and models
+# ==============================================================================
+# Tests 
+# ==============================================================================
+### 2 Common factors -----------------------------------------------------------
+## Load data
+load(file = "../data/1_DGPs_models_pop_params.RData")
 
 ## Draw data
-
 dat <- MASS::mvrnorm(200, rep(0, 6), Sigma = Sigma$Sigma, empirical = TRUE)
 
 ## Estimate
-
 res <-  csem(dat, model_Sigma)
+res_sum <- summarize(res)
 
-# Compare
-compare(res, .what = "Path_estimates", pop_params_Sigma$Path_coefficients)
-compare(res, .what = "Loading_estimates", pop_params_Sigma$Loadings)
+### Test
 
+test_that("No data/model provided should give an error", {
+  expect_error(
+    csem(.model = model)
+  )
+  expect_error(
+    csem(.data = dat)
+  )
+})
 
-
-test_that("Linear model: correctly specified models are correctly returned", {
-  expect_s3_class(parseModel(model4), "cSEMModel")
-
+test_that("Linear model: correctly specified models are correctly estimated", {
+  expect_equal(res_sum$Estimates$Path_estimates$Estimate, pop_params_Sigma$Path_coefficients)
+  expect_equal(res_sum$Estimates$Loading_estimates$Estimate, unname(pop_params_Sigma$Loadings))
 })
