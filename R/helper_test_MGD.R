f436b35b7ae9dc71e137d94dd44ab40860169e40
Manuel Rademaker <manuel.rademaker@uni-wuerzburg.de>
2019-07-09 09:44:16 +0200

    add annoying drop = FALSE

--- R/helper_test_MGD.R
+++ R/helper_test_MGD.R
@@ -417,13 +417,13 @@ calculateFR <- function(.resample_sarstedt) {
   
   x <- .resample_sarstedt[order(.resample_sarstedt[, "group_id"]), ]
 
-  mean_all <- colMeans(x[, names_param])
-  mean_group <- aggregate(x[, names_param], by = list(x[, "group_id"]), FUN = mean)
+  mean_all <- colMeans(x[, names_param, drop = FALSE])
+  mean_group <- aggregate(x[, names_param, drop = FALSE], by = list(x[, "group_id"]), FUN = mean)
   
-  mean_matrix <- apply(mean_group[, -1], 2, function(y) rep(y, times = table(x[, "group_id"])))
+  mean_matrix <- apply(mean_group[, -1, drop = FALSE], 2, function(y) rep(y, times = table(x[, "group_id"])))
   
   SS_between <- rowSums((t(mean_matrix) - mean_all)^2) / (G -1)
-  SS_within  <- colSums((x[, names_param] - mean_matrix)^2) / (B - G)
+  SS_within  <- colSums((x[, names_param, drop = FALSE] - mean_matrix)^2) / (B - G)
   # Note: in the original paper (B -1) was used. This is incorrect. It should
   # be B - G (G = the number of groups). See the F-statistic derived from ANOVA
   # for comparision.
