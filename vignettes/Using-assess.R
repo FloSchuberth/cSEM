5a5b447109bbcbc3d4d85ba062c4c14ee4cbdc3a
Manuel Rademaker <manuel.rademaker@uni-wuerzburg.de>
2020-01-16 12:57:59 +0100

    correct and update vignettes and README

--- vignettes/Using-assess.R
+++ vignettes/Using-assess.R
@@ -1,14 +1,16 @@
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  assess(
 #    .object              = NULL,
 #    .only_common_factors = TRUE,
-#    .quality_criterion   = c("all", "ave", "rho_C", "rho_C_weighted", "cronbachs_alpha",
-#                             "cronbachs_alpha_weighted", "dg", "dl", "dml", "df",
-#                             "esize", "cfi", "gfi", "ifi", "nfi", "nnfi",
-#                             "rmsea", "rms_theta", "srmr",
-#                             "gof", "htmt", "r2", "r2_adj",
-#                             "rho_T", "rho_T_weighted", "vif",
-#                             "vifmodeB",  "fl_criterion"),
+#    .quality_criterion   = c("all", "ave", "rho_C", "rho_C_mm", "rho_C_weighted",
+#                             "rho_C_weighted_mm", "cronbachs_alpha",
+#                            "cronbachs_alpha_weighted", "dg", "dl", "dml", "df",
+#                            "esize", "cfi", "gfi", "ifi", "nfi", "nnfi",
+#                            "reliability",
+#                            "rmsea", "rms_theta", "srmr",
+#                            "gof", "htmt", "r2", "r2_adj",
+#                            "rho_T", "rho_T_weighted", "vif",
+#                            "vifmodeB",  "fl_criterion"),
 #    ...
 #  )
 
