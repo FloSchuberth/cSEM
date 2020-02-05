21e5512de43079297bf4b4b9102a59fd13b178f7
FloSchuberth <f.schuberth@utwente.nl>
2020-02-05 10:29:09 +0100

    Calculate unstadardized scores; default objects it should work, however, it needs to be adjusted for special cases.

---
+++ R/000_new.R
@@ -0,0 +1,17 @@
+calclateUnstandardizedConstructScores=function(.object){
+  
+  dataScaled=.object$Information$Data
+  
+  # Scale data back
+  dataUnscaled=dataScaled * attr(dataScaled, 'scaled:scale') + attr(dataScaled, 'scaled:center') 
+  
+  #Unstandardized scores equal X_i%*%w_i/sum(w_i)
+  
+  w_transformed <- t(.object$Estimates$Weight_estimates)%*%solve(diag(rowSums(.object$Estimates$Weight_estimates)))
+  
+  ScoresUnstandardized <- dataUnscaled%*%w_transformed
+  
+  colnames(ScoresUnstandardized) =  rownames(.object$Estimates$Weight_estimates)
+  
+  return(ScoresUnstandardized)
+}
