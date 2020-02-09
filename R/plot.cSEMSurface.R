006c2728a9b8b724701454f13c434ebb999a3469
FloSchuberth <f.schuberth@utwente.nl>
2020-02-09 22:30:48 +0100

    intermediate

--- R/plot.cSEMSurface.R
+++ R/plot.cSEMSurface.R
@@ -7,15 +7,35 @@
 #' @param x An R object of class `cSEMSurface`.
 #' @param ... Currently ignored.
 #' @export
-plot.cSEMFloodlight <- function(x, ...) {
-  
-  ## Install ggplot2 if not already installed
-  if (!requireNamespace("plotly", quietly = TRUE)) {
-    stop2(
-      "Package `plotly` required. Use `install.packages(\"plotly\")` and rerun.")
-  }
-
-  plot1 <- plotly::plot_ly( x = out$x, y = out$z, z = out$y, type = "surface")  
-  options
-  plot1
-}
+# plot.cSEMSurface <- function(x, ...) {
+# 
+#   ## Install ggplot2 if not already installed
+#   if (!requireNamespace("plotly", quietly = TRUE)) {
+#     stop2(
+#       "Package `plotly` required. Use `install.packages(\"plotly\")` and rerun.")
+#   }
+# 
+#   plot1 <- plotly::plot_ly( x = out$x, y = out$z, z = out$y, type = "surface")
+#   options
+#   plot1
+#   
+#   
+#   # see rsm package for other plots
+#   
+#   # Needs to be added to dependencies
+#   library(plotly)
+#   # For plotting 3D figures options(viewer=NULL) must be set, perhaps there is a more elegant way
+#   
+#   plot_ly( x = ret$x, y = ret$z, z = ret$y, type = "scatter3d",mode='lines')
+#   
+#   scatter3d, mode='lines'.
+#   
+#   p1 <- plot_ly(x= ret$x, y=ret$y,z = ret$z, scene='scene1', lighting = list(ambient = 0.2)) %>%
+#     +   add_surface(showscale=FALSE)
+#   
+#   p1 = plot_ly(ret, x = ~x, y = ~y, z = ~z) 
+#   %>%
+#     +   add_surface(p1,showscale=FALSE)
+#   
+#   
+# }
--- R/postestimate_doSurfaceAnalysis.R
+++ R/postestimate_doSurfaceAnalysis.R
@@ -61,11 +61,13 @@ doSurfaceAnalysis <- function(
     m   <- .object$Information$Model
     est <- .object$Estimates$Estimates_resample$Estimates1$Path_estimates
     H   <- .object$Estimates$Construct_scores
+    Q  <- .object$Estimates$Reliabilities
   } else {
     m   <- .object$Second_stage$Information$Model
     est <- .object$Second_stage$Information$Resamples$Estimates$Estimates1$Path_estimates
     H   <- .object$Second_stage$Estimates$Construct_scores
-  }
+  # add reliabilities 
+    }
   
   ## Check if model is nonlinear.
   if(m$model_type != "Nonlinear"){
@@ -115,54 +117,34 @@ doSurfaceAnalysis <- function(
   # Effects all
   effects_all <- sum_object$Estimates$Path_estimates
   
-  ## Filter interaction, and quadratic terms:
-  # Select the terms that either include x or z
+  nonlinear_vars=indep_vars[grepl(pattern = '.',x = indep_vars,fixed = T)]
   
-  # if there are terms that include not only x and z but a third variable stop
-  
-  
-  # Output: All interaction terms where the dependent and the moderator are 
-  # included including quadratic and higher order polynomials
-  
-  
-  # Select the corresponidng effects
+
+        
+      
   
   
   
-  # Select all relevant effect names 
-  beta_z  <- paste(.dependent,.independent, sep = ' ~ ')
+  # Grab quadratic independent
+  quad_ind=nonlinear_vars[grepl(paste0(.independent,'.',.independent),nonlinear_vars)]
+  quad_mod=nonlinear_vars[grepl(paste0(.moderator,'.',.moderator),nonlinear_vars)]
   
+  # interactions of the .independent and the .moderator
+  grepl(paste(c(.moderator, .independent),collapse = '&'  ),nonlinear_vars)
+  pointer=sapply(indep_vars,function(x){
+    temp=unlist(strsplit(x,'\\.'))
+    if(sum(grepl(paste(c(.moderator, .independent),collapse = '|'  ),temp))==length(temp)){
+      TRUE
+    }else{
+      FALSE
+    }
+  })
   
-  # Possible names of the interaction terms
-  if(!is.null(.moderator)){
-    # Effect name of the moderator variable
-    beta_x  <- paste(.dependent,.moderator, sep = ' ~ ')
-    
-    possible_names_int <- c(paste(.moderator,.independent, sep = '.'), paste(.independent, .moderator, sep= '.'))
-    
-    # Name of the interaction term
-    xz <- possible_names_int[possible_names_int %in% indep_vars]
-    
-    # if(length(xz) != 1){
-    #   stop2(
-    #     "The defined interaction term does not exist in the model or is not",
-    #     " part of the equation of the dependent variable.")
-    # }
-    
-    # effect name
-    beta_xz <- paste(.dependent, xz, sep = ' ~ ')
-    
-  } 
+  vars_rel=indep_vars[pointer]
   
+  sum_rel=sum_object$Estimates$Path_estimates[sum_object$Estimates$Path_estimates$Name%in% 
+                                        paste(.dependent,"~",vars_rel),]
   
-  # name of the quadratic term
-  zz <- paste(.quadratic,.quadratic, sep = '.')
-  beta_zz <- paste(.dependent, zz, sep = ' ~ ')
-  # }
-  
-  # Hier eventuell adjustment von aussen zulassen 
-  ## Compute steps for the moderator and the independent variable
-  # steps <- list(
   steps_ind = seq(min(H[, .independent]), max(H[, .independent]), 
                   length.out = .n_steps)
   steps_mod = seq(min(H[, .moderator]), max(H[, .moderator]), 
@@ -170,71 +152,47 @@ doSurfaceAnalysis <- function(
   
   steps_independent = rep(steps_ind,each=length(steps_mod))
   steps_moderator = rep(steps_mod,times = length(steps_ind))
-  # Create list with steps
   
+  steps=cbind(steps_independent,steps_moderator)
+  colnames(steps)=c(.independent,.moderator)
   
-  # Calculate the predicted values of the dependent variable
-  predicted_dependent <- effects_all[effects_all$Name==beta_z,]$Estimate * steps_independent+
-    effects_all[effects_all$Name==beta_x,]$Estimate  * steps_moderator+
-    effects_all[effects_all$Name==beta_zz,]$Estimate  * (steps_independent^2-1)
+  y_pred_list=lapply(sum_rel$Name,function(x){
+    temp=sum_rel[sum_rel$Name==x,]
   
-  effects_all[effects_all$Name==beta_xz,]$Estimate  * (steps_moderator *steps_independent 
-                                                       - mean(steps_moderator *steps_independent))+
-    effects_all[effects_all$Name==beta_zz,]$Estimate  * (steps_independent^2-1)+
-    effects_all[effects_all$Name==beta_xx,]$Estimate  * (steps_moderator^2-1)
+    indep_names=unlist(strsplit(temp$Name,paste(.dependent,"~ ")))[2]
+    indep_rel=unlist(strsplit(indep_names,'\\.'))
+    
+    if(temp$Construct_type == "Common factor"){
+       average=mean(matrixStats::rowProds(H[,indep_rel,drop=FALSE]))/prod(Q[indep_rel])
+    
+       }else if (temp$Construct_type == "Composite"){
+        average=mean(matrixStats::rowProds(H[,indep_rel,drop=FALSE]))
+       }
+    
+    temp$Estimate*(matrixStats::rowProds(steps[,indep_rel,drop=FALSE])-average)  
+    }
+    
+    )
   
-  # Value of the originally estimated effect of z on y at different levels of x
-  # effect_original <- est$Original[beta_z] + est$Original[beta_xz] * step
-  
-  # Compute empirical quantile based on resamples
-  # bounds <- quantile(effect_resampled , c(.alpha/2, 1 - .alpha/2))
+  y_pred = Reduce('+',y_pred_list)
   
   # Return output
-  out <- list(y=matrix(predicted_dependent,nrow=length(steps_ind),ncol=length(steps_mod)),
+  out <- list(y=matrix(y_pred,nrow=length(steps_ind),ncol=length(steps_mod)),
               z=steps_ind,x=steps_mod)
   
-  
-  # Determine Johnson-Neyman point 
-  # Look for sign flips in the upper boundary
-  # pos_ub <- which(diff(sign(out[, 'ub'])) != 0)
-  # pos_lb <- which(diff(sign(out[, 'lb'])) != 0) 
-  
-  # # Prepare and return output
-  # out <- list(
-  #   "out"                   = out, 
-  #   "Johnson_Neyman_points" = list(
-  #     JNlb = c(x = out[,'value_z'][pos_lb], 
-  #              y = out[,'lb'][pos_lb]),
-  #     JNub = c(x = out[,'value_z'][pos_ub], 
-  #              y = out[,'ub'][pos_ub]
-  #     )
-  #   ),
-  #   # NEEDS TO BE DONE ADD ALL THE NAMES
-  #   "Information" = list(
-  #     alpha       = .alpha, 
-  #     dependent   = .dependent,
-  #     independent =.independent,
-  #     moderator   = .moderator
-  #   )
-  # )
+  # Prepare and return output
+  out <- list(
+    "out"                   = out,
+    "Information" = list(
+      alpha       = .alpha,
+      dependent   = .dependent,
+      independent =.independent,
+      moderator   = .moderator,
+      all_independent = vars_rel 
+    )
+  )
   
   class(ret) <- "cSEMSurface"
   return(ret)
 }
 
-# see rsm package for other plots
-
-# Needs to be added to dependencies
-library(plotly)
-# For plotting 3D figures options(viewer=NULL) must be set, perhaps there is a more elegant way
-
-plot_ly( x = ret$x, y = ret$z, z = ret$y, type = "scatter3d",mode='lines')
-
-scatter3d, mode='lines'.
-
-p1 <- plot_ly(x= ret$x, y=ret$y,z = ret$z, scene='scene1', lighting = list(ambient = 0.2)) %>%
-  +   add_surface(showscale=FALSE)
-
-p1 = plot_ly(ret, x = ~x, y = ~y, z = ~z) 
-%>%
-  +   add_surface(p1,showscale=FALSE)
