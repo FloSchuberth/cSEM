abc5e92db22d2b89fe8ff36732d0b78da5e527ff
Manuel Rademaker <manuel.rademaker@uni-wuerzburg.de>
2020-03-28 11:21:21 +0100

    fixed #342

--- R/00_foreman.R
+++ R/00_foreman.R
@@ -109,6 +109,9 @@ foreman <- function(
       .approach_gcca            = .approach_weights
     )
   } else if(.approach_weights == "GSCA") {
+    if(csem_model$model_type == "Nonlinear") {
+      stop2("cSEM currently does not support GSCA and GSCAm for models containing nonlinear terms.")
+    }
     if(.disattenuate & all(csem_model$construct_type == "Common factor")) {
       W <- calculateWeightsGSCAm(
         .X                        = X,
