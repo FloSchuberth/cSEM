020739a701257b2fee051e8e22bc0546219cf6a3
Manuel Rademaker <manuel.rademaker@uni-wuerzburg.de>
2020-05-11 11:02:36 +0200

    intermediate commit

--- R/print.cSEMNonlinearEffects.R
+++ R/print.cSEMNonlinearEffects.R
@@ -18,8 +18,12 @@ print.cSEMNonlinearEffects <- function(x, ...) {
     rule2("Simple slope analysis", type = 1, align = "center")
   )
   
+  # Questions
+  # 1. Does Information_print always look the same
+  # 
+  # 2. 
   x <- neffects
-  
+  neffects$Information_print
   info  <- x$Information_print
   dep   <- x$Information$dependent
   indep <- x$Information$independent
@@ -31,7 +35,13 @@ print.cSEMNonlinearEffects <- function(x, ...) {
   
   
   # Slope when moderator = mean(=0)-2SD
-  cat2("\n\nSlope when", mod, " = ? (-2 SD)")
+  cat2("\n\nSlope of ", dep, " when ", mod, " = ? (-2 SD from mean):")
+  cat2(
+    "\n  ", 
+    col_align("Direct effect", 10, align = "right"), 
+    col_align("Std. error", 12, align = "right"),
+    col_align("p-value", 10, align = "right")
+  )
   x$Information_print[x$Information_print[,'value_z']==-2,]['direct_effect']
   # CI
   # lb:
@@ -39,6 +49,7 @@ print.cSEMNonlinearEffects <- function(x, ...) {
   # ub:
   x$Information_print[x$Information_print[,'value_z']==-2,]['ub']
   
+  cat2("\n\nSlope when ", mod, " = ? (-1 SD):")
   # Slope when moderator = mean(=0)-1SD
   x$Information_print[x$Information_print[,'value_z']==-1,]['direct_effect']
   # CI
@@ -47,7 +58,7 @@ print.cSEMNonlinearEffects <- function(x, ...) {
   # ub:
   x$Information_print[x$Information_print[,'value_z']==-1,]['ub']
   
-  # Slope when moderator at mean (=0)
+  cat2("\n\nSlope when ", mod, " = ? (mean):")
   x$Information_print[x$Information_print[,'value_z']==0,]['direct_effect']
   # CI
   # lb:
