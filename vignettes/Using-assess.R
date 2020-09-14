8c4de285d6452155482f48c8de34a78b5892ecc9
Manuel Rademaker <manuel.rademaker@uni-wuerzburg.de>
2020-09-14 14:17:03 +0200

    fix double arguments in testMGD()

--- vignettes/Using-assess.R
+++ vignettes/Using-assess.R
@@ -2,16 +2,17 @@
 #  assess(
 #    .object              = NULL,
 #    .only_common_factors = TRUE,
-#    .quality_criterion   = c("all", "ave", "rho_C", "rho_C_mm", "rho_C_weighted",
-#                             "rho_C_weighted_mm", "cronbachs_alpha",
-#                             "cronbachs_alpha_weighted", "dg", "dl", "dml", "df",
-#                             "f2", "chi_square", "chi_square_df",
+#    .quality_criterion   = c("all", "aic", "aicc", "aicu", "bic", "fpe", "gm", "hq",
+#                             "hqc", "mallows_cp", "ave",
+#                             "rho_C", "rho_C_mm", "rho_C_weighted",
+#                             "rho_C_weighted_mm", "dg", "dl", "dml", "df",
+#                             "effects", "f2", "fl_criterion", "chi_square", "chi_square_df",
 #                             "cfi", "gfi", "ifi", "nfi", "nnfi",
 #                             "reliability",
 #                             "rmsea", "rms_theta", "srmr",
 #                             "gof", "htmt", "r2", "r2_adj",
 #                             "rho_T", "rho_T_weighted", "vif",
-#                             "vifmodeB",  "fl_criterion"),
+#                             "vifmodeB"),
 #    ...
 #  )
 
