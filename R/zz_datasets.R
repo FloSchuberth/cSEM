b19cd5e7766b2abb6496f588687927458e8706b6
Schuberth <f.schuberth@utwente.nl>
2021-11-05 19:57:16 +0100

    add exampe to Bergami dataset close #456

--- R/zz_datasets.R
+++ R/zz_datasets.R
@@ -512,7 +512,7 @@
 #' The dataset contains 22 variables and originates 
 #' from a larger survey among South Korean employees conducted and
 #' reported by \insertCite{Bergami2000;textual}{cSEM}. It is
-#' also used in  \insertCite{Hwang2014;textual}{cSEM} and 
+#' also used in  \insertCite{Hwang2004;textual}{cSEM} and 
 #' \insertCite{Henseler2020;textual}{cSEM} 
 #' for demonstration purposes, see the corresponding tutorial.
 #' 
@@ -524,7 +524,7 @@
 #' #============================================================================
 #' # Example is taken from Henseler (2020)
 #' #============================================================================
-#' model_Bergami_Bagozzi="
+#' model_Bergami_Bagozzi_Henseler="
 #' # Measurement models
 #' OrgPres =~ cei1 + cei2 + cei3 + cei4 + cei5 + cei6 + cei7 + cei8 
 #' OrgIden =~ ma1 + ma2 + ma3 + ma4 + ma5 + ma6
@@ -538,10 +538,35 @@
 #' AffJoy  ~ OrgPres + OrgIden + Gender 
 #' "
 #' 
-#' out <- csem(.data = BergamiBagozzi2000, .model = model_Bergami_Bagozzi,
+#' out <- csem(.data = BergamiBagozzi2000, 
+#'             .model = model_Bergami_Bagozzi_Henseler,
 #'             .PLS_weight_scheme_inner = 'factorial',
 #'             .tolerance = 1e-06
 #' )
+#' 
+#' #============================================================================
+#' # Example is taken from Hwang et al. (2004)
+#' #============================================================================ 
+#' 
+#' model_Bergami_Bagozzi_Hwang="
+#' # Measurement models
+#' OrgPres =~ cei1 + cei2 + cei3 + cei4 + cei5 + cei6 + cei7 + cei8 
+#' OrgIden =~ ma1 + ma2 + ma3 + ma4 + ma5 + ma6
+#' AffJoy =~ orgcmt1 + orgcmt2 + orgcmt3 + orgcmt7
+#' AffLove  =~ orgcmt5 + orgcmt5 + orgcmt8
+#' 
+#' # Structural model 
+#' OrgIden ~ OrgPres 
+#' AffLove ~ OrgIden
+#' AffJoy  ~ OrgIden"
+#'
+#'out_Hwang <- csem(.data = BergamiBagozzi2000, 
+#'                  .model = model_Bergami_Bagozzi_Hwang,
+#'                  .approach_weights = "GSCA",
+#'                  .disattenuate = FALSE,
+#'                  .id = "gender",
+#'                  .tolerance = 1e-06) 
+#'
 #' 
 #' @references
 #'   \insertAllCited{}
