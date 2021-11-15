677037430066ca9f42d38c1d8d190a4056ad394c
FloSchuberth <f.schuberth@utwente.nl>
2021-11-15 08:42:25 +0100

    Add HTMT2 to exporttoexcel()

--- R/exportToExcel.R
+++ R/exportToExcel.R
@@ -170,7 +170,12 @@ exportToExcel <- function(
     openxlsx::writeData(wb, sheet = "HTMT", if(is.null(.postestimation_object$HTMT)) {
       NA
     } else {
-      .postestimation_object$HTMT
+      .postestimation_object$HTMT$out_print
+    })
+    openxlsx::writeData(wb, sheet = "HTMT2", if(is.null(.postestimation_object$HTMT2)) {
+      NA
+    } else {
+      .postestimation_object$HTMT2$out_print
     })
     openxlsx::writeData(wb, sheet = "Fornell-Larcker matrix", if(is.null(.postestimation_object$`Fornell-Larcker`)) {
       NA
