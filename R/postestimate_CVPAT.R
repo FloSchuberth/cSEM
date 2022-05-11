fa0ff2cff450438035f5db579fb72aebc7d93736
Tamara Schamberger <tamara.schamberger@uni-wuerzburg.de>
2022-05-11 10:08:24 +0200

    Rename functions

--- R/postestimate_CVPAT.R
+++ R/postestimate_CVPAT.R
@@ -16,7 +16,7 @@
 #'                            both models is identical.}
 #'     }
 #'   
-#' @usage postestimate_CVPAT(
+#' @usage testCVPAT(
 #' .object1  = NULL,
 #' .object2  = NULL,
 #' .seed     = NULL,
@@ -35,7 +35,7 @@
 #' 
 #' @export
 
-postestimate_CVPAT <- function(
+testCVPAT <- function(
   .object1  = NULL,
   .object2  = NULL,
   .seed     = NULL,
@@ -49,23 +49,24 @@ postestimate_CVPAT <- function(
   
   
   #Stop if one object is second order
+  #TestCVPAT() uses predict() which is not implemented for models containing higher-order constructs
   if(inherits(.object1, "cSEMResults_2ndorder")|inherits(.object2, "cSEMResults_2ndorder")) {
-    stop2('Currently, `predict()` is not implemented for models containing higher-order constructs.')
+    stop2('Currently, `testCVPAT()` is not implemented for models containing higher-order constructs.')
   }
   
   #Stop if one object is not admissible
   if(sum(verify(.object1))!=0|sum(verify(.object2))!=0) {
-    stop2('One csem object is not admissible.')
+    stop2('One cSEM object is not admissible.')
   }
   
   #Stop if one object does not have a structural model
   if(all(.object1$Information$Model$structural == 0)|all(.object2$Information$Model$structural == 0)) {
-    stop2("`predict()` requires a structural model.")
+    stop2("`testCVPAT()` requires a structural model.")
   }
   
   #Stop if one object contains a non-linear model
   if(.object1$Information$Model$model_type != 'Linear'|.object2$Information$Model$model_type != 'Linear'){
-    stop2('Currently, `predict()` works only for linear models.')
+    stop2('Currently, `testCVPAT()` works only for linear models.')
   }
   
   #Stop if both objects are based on different datasets
@@ -75,8 +76,10 @@ postestimate_CVPAT <- function(
   
   #Stop if no seed is provided
   if(is.null(.seed)){
-    stop2('postestimate_CVPAT() requires a seed.')
+    warning2('`testCVPAT()` requires a seed. Since no seed has been provided a random seed is used.')
+    .seed = sample(1:1000,1)
   }
+  
   
   #Perform out-of-sample predictions for both models
   predict1 <- predict(.object = .object1, .benchmark = "lm", 
@@ -85,15 +88,18 @@ postestimate_CVPAT <- function(
   predict2 <- predict(.object = .object2, .benchmark = "lm", 
                       .cv_folds = .cv_folds, .r = 1, .seed = .seed)
   
-  L1 <- unlist(predict1$Residuals_target[[1]])
-  L2 <- unlist(predict2$Residuals_target[[1]])
+  L1 <- c(as.matrix(predict1$Residuals_target[[1]]))
+  L2 <- c(as.matrix(predict2$Residuals_target[[1]]))
   
-  D_bar <- mean(L2 - L1)
+  D <- L2 - L1
   
-  test_stat <- D_bar/sqrt(var(L2-L1)/length(L1))
+  D_bar <- mean(D)
+  
+  test_stat <- D_bar/sqrt(var(D)/length(L1))
   
   p_value <- 2*(1-pt(abs(test_stat), length(L1)-1))
   
+  #missing: degrees of freedom, decision, information: seed, number cv_folds, alpha
   out <- list(
     "test_statistic" = test_stat,
     "p-value"        = p_value
