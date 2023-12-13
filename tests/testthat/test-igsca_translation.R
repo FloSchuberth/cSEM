81ae10ceff2c09b7f52f0b1915b1937c8f642bf4
emstruong <112967876+emstruong@users.noreply.github.com>
2023-12-12 19:46:05 -0500

    Alpha version of translation

---
+++ tests/testthat/test-igsca_translation.R
@@ -0,0 +1,8 @@
+
+
+
+
+write.csv(igsca_sim_out$mW, file = "~/Documents/RStudio/cSEM/dev/Notes/mW_R_Dec3_2023.csv")
+write.csv(igsca_sim_out$mC, file = "~/Documents/RStudio/cSEM/dev/Notes/mC_R_Dec3_2023.csv")
+write.csv(igsca_sim_out$mB, file = "~/Documents/RStudio/cSEM/dev/Notes/mB_R_Dec3_2023.csv")
+write.csv(igsca_sim_out$mD, file = "~/Documents/RStudio/cSEM/dev/Notes/mD_R_Dec3_2023.csv")
