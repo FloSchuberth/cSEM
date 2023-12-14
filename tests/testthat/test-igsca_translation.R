0521cad6f32888401e1f9f6a9dc94b043132e65a
emstruong <112967876+emstruong@users.noreply.github.com>
2023-12-14 16:26:52 -0500

    Basic refactoring written

--- tests/testthat/test-igsca_translation.R
+++ tests/testthat/test-igsca_translation.R
@@ -1,3 +1,4 @@
+context("igsca_translation")
 
 # TODO: This is very WIP
 
@@ -6,3 +7,8 @@ write.csv(igsca_sim_out$mW, file = "~/Documents/RStudio/cSEM/dev/Notes/mW_R_Dec3
 write.csv(igsca_sim_out$mC, file = "~/Documents/RStudio/cSEM/dev/Notes/mC_R_Dec3_2023.csv")
 write.csv(igsca_sim_out$mB, file = "~/Documents/RStudio/cSEM/dev/Notes/mB_R_Dec3_2023.csv")
 write.csv(igsca_sim_out$mD, file = "~/Documents/RStudio/cSEM/dev/Notes/mD_R_Dec3_2023.csv")
+
+
+
+
+
