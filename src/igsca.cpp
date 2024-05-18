a02ca37c7af3c3bd9e9102acb0660db7168d30e8
emstruong <112967876+emstruong@users.noreply.github.com>
2024-05-18 11:04:27 -0400

    Updated namespace to properly use RCPP

--- src/igsca.cpp
+++ src/igsca.cpp
@@ -1,2 +1,8 @@
 #include <Rcpp.h>
 using namespace Rcpp;
+
+// From the documentation of a default rstudio cpp file
+// [[Rcpp::export]]
+NumericVector timesTwo(NumericVector x) {
+  return x * 2;
+}
