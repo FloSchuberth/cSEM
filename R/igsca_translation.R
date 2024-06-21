9d49f354e8d8e144cbcff1311e2e5c79dc06f0f9
FloSchuberth <f.schuberth@utwente.nl>
2024-06-21 16:05:14 +0200

    revert Formatted function and removed browser

--- R/igsca_translation.R
+++ R/igsca_translation.R
@@ -1010,7 +1010,8 @@ parse_GSCAPro_FullResults <-
     "GSCAPro_1_2_1Output",
     "Tutorial_IGSCA_model1_full_result_ver0.csv"
   ),
-  headers = c(# "Model fit measures",
+  headers = c(
+    # "Model fit measures",
     "Weights",
     "Loadings",
     "Path coefficients",
@@ -1024,11 +1025,16 @@ parse_GSCAPro_FullResults <-
     # "F squared values",
     # "Sample correlations (lower diagonal) & Residual correlations (upper diagonal)",
     # "Correlations between indicators and component/correlations",
-    "Unique D^2")) {
-    file <- here::here(file_path)
+    "Unique D^2"
+  )) {
     
-    to_parse <-
-      data.table::fread(file = file, fill = TRUE, sep = ",")
+    browser()  
+    # tables <- lapply(headers, FUN = \(x) try(data.table::fread(file = file, fill = TRUE, skip = x, sep = ",",
+    #                                                            blank.lines.skip = TRUE)))
+    
+    file <- here::here(file_path)  
+    
+    to_parse <- data.table::fread(file = file, fill = TRUE, sep = ",")
     
     col_1_strings <- unlist(to_parse[, 1])
     idx_of_headers <- which((col_1_strings %in% headers) == TRUE)
@@ -1036,15 +1042,13 @@ parse_GSCAPro_FullResults <-
     idx_of_blank_rows <- which((col_1_strings %in% "") == TRUE)
     
     # Gets the idx of idx_of_blank_rows of the first blank that occurs after the header
-    list_of_blanks <-
-      lapply(idx_of_headers, FUN = \(x) which.max(x < idx_of_blank_rows))
-    idx_first_blank_after_header <-
-      lapply(list_of_blanks, FUN = \(x) idx_of_blank_rows[x]) |>
+    list_of_blanks <- lapply(idx_of_headers, FUN = \(x) which.max(x < idx_of_blank_rows))
+    idx_first_blank_after_header <- lapply(list_of_blanks, FUN = \(x) idx_of_blank_rows[x]) |>
       unlist()
     
     # Add one to headers because the first row with the column names is one row after the header name
     nrows_of_tables <-
-      idx_first_blank_after_header - (idx_of_headers + 1)
+      idx_first_blank_after_header - (idx_of_headers + 1) 
     
     if (length(idx_of_headers) != length(nrows_of_tables)) {
       stop("The number of rows and the the number of indices of headers does not match")
@@ -1054,10 +1058,8 @@ parse_GSCAPro_FullResults <-
       FUN = data.table::fread,
       file = file,
       fill = TRUE,
-      skip = idx_of_headers + 1,
-      # Add one because in Version 1.2.1 of GSCA Pro the Header was one row ahead of the column labels
-      nrows = nrows_of_tables,
-      # Minus one for adjusting for the idx_of_headers and another for the empty-gaps between tables
+      skip = idx_of_headers+1, # Add one because in Version 1.2.1 of GSCA Pro the Header was one row ahead of the column labels
+      nrows = nrows_of_tables, # Minus one for adjusting for the idx_of_headers and another for the empty-gaps between tables
       sep = ","
     )
     names(tables) <- headers
