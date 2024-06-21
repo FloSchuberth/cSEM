b025afb83dfae5fd026b147ddf7cc6186256465b
FloSchuberth <f.schuberth@utwente.nl>
2024-06-21 15:11:29 +0200

    revert Added RCPPArmadillo functionality

--- R/cSEM-package.R
+++
@@ -1,9 +0,0 @@
-## usethis namespace: start
-#' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
-
-## usethis namespace: start
-#' @useDynLib cSEM, .registration = TRUE
-## usethis namespace: end
-NULL
