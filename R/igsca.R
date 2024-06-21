bfdabd88e1f5c29cd36c365688ab2d1db582161e
FloSchuberth <f.schuberth@utwente.nl>
2024-06-21 15:30:12 +0200

    Revert "Added list2env in initialization of igsca"

--- R/igsca.R
+++ R/igsca.R
@@ -353,9 +353,10 @@ prepare_for_ALS <- function(Z0, W0, B0, n_indicators, n_case, n_constructs, ov_t
       W0 = apply(W0 != 0, 2, as.numeric),
       B0 = apply(B0 != 0, 2, as.numeric)
     )
-  
-  list2env(initial_est[c("W", "C", "B")], envir = environment())
-  
+  # TODO: Update using list2env()
+  W <- initial_est$W
+  C <- initial_est$C
+  B <- initial_est$B
   
   
   V <- cbind(diag(n_indicators), W)
