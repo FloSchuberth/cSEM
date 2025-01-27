4987a3724d1616c2308ba9a832d4f83b2879c8ba
FloSchuberth <f.schuberth@utwente.nl>
2025-01-27 13:13:01 +0100

    Add plotModel, a function that visualizes cSEM models. Thanks to Nguyen Huu Phuc.

---
+++ inst/examples/example_plotModel.R
@@ -0,0 +1,22 @@
+\dontrun{
+model_Bergami_int="
+  # Common factor and composite models
+  OrgPres <~ cei1 + cei2 + cei3 + cei4 + cei5 + cei6 + cei7 + cei8 
+  OrgIden =~ ma1 + ma2 + ma3 + ma4 + ma5 + ma6
+  AffJoy =~ orgcmt1 + orgcmt2 + orgcmt3 + orgcmt7
+  AffLove  =~ orgcmt5 + orgcmt6 + orgcmt8
+
+  # Structural model 
+  OrgIden ~ OrgPres 
+  AffLove ~ OrgPres+OrgIden+OrgPres.OrgIden
+  AffJoy  ~ OrgPres+OrgIden
+  "
+  
+  outBergamiInt <- csem(.data = BergamiBagozzi2000,.model = model_Bergami_int,
+                        .disattenuate = T,
+                        .PLS_weight_scheme_inner = 'factorial',
+                        .tolerance = 1e-6,
+                        .resample_method = 'none')
+  
+  plotModel(outBergamiInt)
+}
