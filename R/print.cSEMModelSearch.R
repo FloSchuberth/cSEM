ca347b33666eee8fd1fdf88e53e0fc0a695cb6bc
FloSchuberth <f.schuberth@utwente.nl>
2026-01-16 10:31:01 +0100

    Start print method for doModelSearch

---
+++ R/print.cSEMModelSearch.R
@@ -0,0 +1,13 @@
+#' `cSEMModelSearch` method for `print()`
+#'
+#' The `cSEMModelSearch` method for the generic function [print()]. 
+#'
+#' @inheritParams csem_arguments
+#'
+#' @seealso [csem()], [cSEMResults], [doModelSearch()]
+#'
+#' @export
+#' @keywords internal
+print.cSEMModelSearch <- function(x, ...) {
+  
+}
