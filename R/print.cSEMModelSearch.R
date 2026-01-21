5beb7a4ef263a3bc42e1799c8a21c308363c998b
FloSchuberth <f.schuberth@utwente.nl>
2026-01-21 14:32:22 +0100

    intermediate

--- R/print.cSEMModelSearch.R
+++ R/print.cSEMModelSearch.R
@@ -11,23 +11,41 @@
 print.cSEMModelSearch <- function(x, ...) {
   cat2(
     rule2(type = 2), "\n",
-    rule2("Overview AGAS-PLS")
+    rule2("Overview")
   )
-  
+  cat2("\n\tAGAS-PLS information:\n\t","------------------------")
   cat2(
-    col_align("\n\n\tPopulation size", width = 36), "= ", 
+    col_align("\n\tPopulation size", width = 36), "= ", 
     x$Information$pop_size,
-    col_align("\n\n\tNumber of generations", width = 36), "= ", 
+    col_align("\n\tNumber of generations", width = 36), "= ", 
     x$Information$n_generation,
-    col_align("\n\n\tMutation probability", width = 36), "= ", 
+    col_align("\n\tMutation probability", width = 36), "= ", 
     x$Information$prob_mutation,
-    col_align("\n\n\tCrossover probability", width = 36), "= ", 
+    col_align("\n\tCrossover probability", width = 36), "= ", 
     x$Information$prob_crossover,
     col_align("\n\tPenalize value (fbar)", width = 36), "= ", 
     format(x$Information$fbar, scientific = FALSE),
-    col_align("\n\n\tModel selection criterion used", width = 36), "= ", 
-    x$Information$model_selection_criterion,
-    col_align("\n\tFitness value of the original model", width = 36), "= ", 
-    x$original_fitness
+    col_align("\n\tModel selection criterion used", width = 36), "= ", 
+    x$Information$model_selection_criterion
   )
+  
+  cat2('\n')
+  
+  cat2("\n\tOriginal model:\n\t","------------------------")
+  cat2(col_align("\n\tFitness value", width = 36), "= ", 
+       x$original_fitness)
+  
+  cat2('\n')
+  
+  cat2("\n\tAGAS-PLS results:\n\t","------------------------")
+  cat2(
+    col_align("\n\tFitness value", width = 36), "= ", 
+    x$best_fitness,
+    col_align("\n\tNumber of models found", width = 36), "= ", 
+    length(x$best_matrix),
+    col_align("\n\tStructure of the model found", width = 36), "=\n ", 
+    x$bext_matrix[[1]]
+  )
+  
+  
 }
