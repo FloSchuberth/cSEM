0aed7e34355e08b03b373195a0140a8efb47366d
emstruong <112967876+emstruong@users.noreply.github.com>
2026-01-22 14:22:03 -0500

    More development scripts

--- dev/igsca/igsca_note.R
+++ dev/igsca/igsca_note.R
@@ -21,30 +21,6 @@ gscam <- csem(
   .dominant_indicators = c("y11", "y21", "y31")
 )
 
-# debugonce(csem)
-# debugonce(resamplecSEMResults)
-debugonce(selectAndVectorize) # TODO: This has to be updated to include D
-debugonce(summarize) # TODO: I may have to go into summarize in-order to get the unique loadings
-debugonce(resamplecSEMResultsCore) # TODO: The actual resampling
-debugonce(resamplecSEMResults) # TODO: This processes the results after resampling
-gscam <- csem(
-  threecommonfactors,
-  model,
-  .approach_weights = "GSCA",
-  .tolerance = 0.0001,
-  .conv_criterion = "sum_diff_absolute",
-  .resample_method = "bootstrap",
-  .R = 5,
-  .dominant_indicators = c("y11", "y21", "y31")
-)
-
-(tidied_gscam <- tidy(gscam, conf.int = TRUE))
-# View(tidied_gscam)
-
-
-# IGSCA ------------------------------------------------------------------
-
-
 model <- "
 # Structural model
 eta2 ~ eta1
@@ -56,6 +32,28 @@ eta2 <~ y21 + y22 + y23
 eta3 =~ y31 + y32 + y33
 "
 
-res <- csem(threecommonfactors, model)
+igsca <- csem(threecommonfactors, model, .approach_weights = "GSCA",
+  .tolerance = 0.0001,
+  .conv_criterion = "sum_diff_absolute",
+  .resample_method = "bootstrap",
+  .R = 5,
+  .dominant_indicators = c("y11", "y21", "y31"))
 
 
+model <- "
+# Structural model
+eta2 ~ eta1
+eta3 ~ eta1 + eta2
+
+# Each concept is measured by 3 indicators, i.e., two are common factors and one is a composite
+eta1 <~ y11 + y12 + y13
+eta2 <~ y21 + y22 + y23
+eta3 <~ y31 + y32 + y33
+"
+
+gsca <- csem(threecommonfactors, model, .approach_weights = "GSCA",
+  .tolerance = 0.0001,
+  .conv_criterion = "sum_diff_absolute",
+  .resample_method = "bootstrap",
+  .R = 5,
+  .dominant_indicators = c("y11", "y21", "y31"))
