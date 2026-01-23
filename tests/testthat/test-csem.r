b3eb4af26d4869dfa63d6906885dd8dffb375a3f
emstruong <112967876+emstruong@users.noreply.github.com>
2026-01-22 20:44:07 -0500

    made test more robust?

--- tests/testthat/test-csem.r
+++ tests/testthat/test-csem.r
@@ -180,12 +180,12 @@ for (i in c(
     .path = testthat::test_path("test_results_exportToExcel")
   )
   exportToExcel(
-    predict(res, .handle_inadmissibles = if (i == "GSCA") "ignore" else "stop", .benchmark = "lm", .disattenuate = FALSE),
+    predict(res, .handle_inadmissibles = 'set_NA', .benchmark = "lm", .disattenuate = FALSE),
     .filename = paste0("test_predict_", i, ".xlsx"),
     .path = testthat::test_path("test_results_exportToExcel")
   )
   exportToExcel(
-    testOMF(res, .R = 10, .handle_inadmissibles = if (i == "GSCA") "ignore" else "drop"),
+    testOMF(res, .R = 10, .handle_inadmissibles = 'drop'),
     .filename = paste0("test_testOMF_", i, ".xlsx"),
     .path = testthat::test_path("test_results_exportToExcel")
   )
