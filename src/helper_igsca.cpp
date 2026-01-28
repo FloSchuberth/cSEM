14c945805eadf53a39ae07304b27afb6a1c08891
FloSchuberth <f.schuberth@utwente.nl>
2026-01-28 14:02:32 +0100

    auto created

--- src/helper_igsca.cpp
+++ src/helper_igsca.cpp
@@ -10,13 +10,3 @@ arma::mat kroneckerC(arma::mat &X, arma::mat &Y, arma::uvec &idx) {
   
   return prod.cols(idx-1);
 }
-
-
-//[[Rcpp::export]]
-arma::sp_mat kroneckerCsp(arma::mat &X, arma::mat &Y, arma::uvec &idx) {
-  // specific to kronecker product of Identity matrix with arbitrary matrix
-  arma::sp_mat prod;
-  prod = arma::kron(X, Y); 
-  
-  return prod.cols(idx - 1);
-}
