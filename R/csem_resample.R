8bece33c3932d8940999b8d3bf8d97a8b4482597
kss <slupphaugkjell@gmail.com>
2026-08-04 20:29:01 +0200

    Check if `.Random.seed` is initialized restoring it in `on.exit()`

--- R/csem_resample.R
+++ R/csem_resample.R
@@ -216,11 +216,14 @@ resampleData <- function(
     }
   }
   
-  # Save old seed and restore on exit! This is important since users may have
+  # Save old seed (if it exists) and restore on exit!
+  # This is important since users may have
   # set a seed before, in which case the global seed would be
-  # overwritten if not explicitly restored
-  old_seed <- .Random.seed
-  on.exit({.Random.seed <<- old_seed})
+  # overwritten if not explicitly restored.
+  if (exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) {
+    old_seed <- get(".Random.seed", envir = .GlobalEnv, inherits = FALSE)
+    on.exit(assign(".Random.seed", old_seed, envir = .GlobalEnv), add = TRUE)
+  }
   
   ## Create seed if not already set
   if(is.null(.seed)) {
@@ -578,12 +581,10 @@ resamplecSEMResults <- function(
   # Save old seed and restore on exit! This is important since users may have
   # set a seed before, in which case the global seed would be
   # overwritten if not explicitly restored
-  # Note (07.10.2020): for some tests performed by testthat .Random.seed is not 
-  #                   available. This is when it is not initialized. To initialize
-  #                   the .Random.seed object a random number is generated.
-  runif(1)
-  old_seed <- .Random.seed
-  on.exit({.Random.seed <<- old_seed})
+  if (exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) {
+    old_seed <- get(".Random.seed", envir = .GlobalEnv, inherits = FALSE)
+    on.exit(assign(".Random.seed", old_seed, envir = .GlobalEnv), add = TRUE)
+  }
   
   ## Create seed if not already set
   if(is.null(.seed)) {
