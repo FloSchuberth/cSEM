0b8bed36ef77ca22d5a129097b4bcd0e889e7b91
FloSchuberth <f.schuberth@utwente.nl>
2026-08-05 10:41:35 +0200

    Fix issue with seed in non-interactive sessions

--- R/postestimate_test_OMF.R
+++ R/postestimate_test_OMF.R
@@ -198,7 +198,7 @@ testOMF <- function(
     # drawing a random seed out of .Random.seed. If set.seed(seed = NULL) is not
     # called sample(.Random.seed, 1) would result in the same random seed as
     # long as .Random.seed remains unchanged. By resetting the seed we make 
-    # sure that sample draws a different element everytime it is called.
+    # sure that sample draws a different element every time it is called.
     .seed <- sample(.Random.seed, 1)
   }
   ## Set seed
